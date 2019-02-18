1145ca94effe2f66353eec887378e0aa3f46e4ba
winniewjeng <winniewjeng@gmail.com>
2019-02-18 14:31:49 -0800

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+function dropFxn() {
+  var x = document.getElementById("dropMenu");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
